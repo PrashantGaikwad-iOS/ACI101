06fcbc802757e58f7f8830aa70b849f14fb546b1
Prashant Gaikwad <itsmeprashantg@gmail.com>
2022-02-10 22:26:03 +0530

    add the description

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -3,7 +3,7 @@ import Foundation
 class Entry: Codable
 {
     var title:   String = "Hello Prashant"
-    var content: String = ""
+    var content: String = "How are you doing?"
     
     var id: String = UUID().uuidString
     
